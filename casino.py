78a2fbdb20f1637e60bf5871b69eddb78f93ae69
IsabellaZa <isabella.zaby@gmail.com>
2018-04-28 18:11:59 +0200

    updated casino with too high too low option

--- casino.py
+++ casino.py
@@ -1,7 +1,11 @@
 secret = 11
-guess = int(raw_input("Guess the Number: "))
 
-if secret == guess :
-    print "Congratulations!"
-else :
-    print "Too bad!"
+for x in range (5):
+    guess = int(raw_input("Guess the Number between 1 and 30: "))
+    if secret == guess :
+        print "Congratulations! It really is 11!"
+        break
+    elif guess > secret:
+        print "You guessed too high!"
+    elif guess < secret:
+        print "You guessed too low!"
