b186436d7893d22e859f37a2f98e90d0e4058010
IsabellaZa <isabella.zaby@gmail.com>
2018-05-07 22:14:59 +0200

    random number and def yes no implemented

--- casino.py
+++ casino.py
@@ -1,11 +1,38 @@
-secret = 11
+import random
+from askyesno import ask_yes_or_no_question
+print "=" * 30
+print "Welcome to the Bella-Casino-Game!"
+secret = random.randint(1,30)
+counter = 0
+while True:
+    while True:
+        while True:
+            try:
+                guess = int(raw_input("Guess the Number between 1 and 30: "))
+                counter += 1
+                break
+            except ValueError:
+                print "You did not enter a number!"
 
-for x in range (5):
-    guess = int(raw_input("Guess the Number between 1 and 30: "))
-    if secret == guess :
-        print "Congratulations! It really is 11!"
+        if secret == guess:
+            print "Congratulations! It really is " + str(secret) +"!"
+            counter = 0
+            break
+        elif counter == 5:
+            print "Sorry! You can only guess 5 times! The answer would have been " + str(secret) + "!"
+            counter = 0
+            break
+        elif guess > secret:
+            print "You guessed too high!"
+        elif guess < secret:
+            print "You guessed too low!"
+
+
+
+    again = ask_yes_or_no_question("Would you like to play again?")
+    if again == False:
+        print "Thank you for playing!"
+        print "END"
+        print "=" * 30
+
         break
-    elif guess > secret:
-        print "You guessed too high!"
-    elif guess < secret:
-        print "You guessed too low!"
